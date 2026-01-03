3ad2c7810bf6bccfea4785986671a90aa03bb801
ajaypal2005 <ap461719@gmail.com>
2026-01-03 20:50:49 +0530

    first commit

---
+++ Basics.cpp
@@ -0,0 +1,10 @@
+// basic syntax for a C++ program
+
+#include <iostream> //header file for input and output
+using namespace std; //namespace declaration
+int main() //execution starts from here
+{
+    cout << "Hello, World!" << endl; //body of the program
+    return 0;
+}
+
