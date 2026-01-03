7b0c2852e6b4e85f40b7b0b29f93dc9c9536ba79
ajaypal2005 <ap461719@gmail.com>
2026-01-03 21:08:54 +0530

    Update Basics.cpp and README, ignore exe

--- Basics.cpp
+++ Basics.cpp
@@ -6,5 +6,28 @@ int main() //execution starts from here
 {
     cout << "Hello, World!" << endl; //body of the program
     return 0;
+
+// variable declaration and initialization
+    int a = 10;
+    float b = 5.5;
+    char c = 'A';
+
+    // output the values
+    cout << "Integer: " << a << endl;
+    cout << "Float: " << b << endl;
+    cout << "Character: " << c << endl;
+
+    return 0;
+
+    //  basic data types in C++
+    int integerVar = 42;               // Integer type
+    float floatVar = 3.14f;           // Floating-point type
+    double doubleVar = 2.718281828;   // Double-precision floating-point type
+    char charVar = 'C';               // Character type     
+    bool boolVar = true;              // Boolean type
+    string stringVar = "Hello, C++";  // String type
+    
+
+    return 0;
 }
 
