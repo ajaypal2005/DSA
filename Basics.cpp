b6a1ff43cffaa1583e00b705ae2e588080ed602e
ajaypal2005 <ap461719@gmail.com>
2026-01-03 21:17:52 +0530

    datatype

--- Basics.cpp
+++ Basics.cpp
@@ -1,13 +1,13 @@
 // basic syntax for a C++ program
 
-#include <iostream> //header file for input and output
-using namespace std; //namespace declaration
-int main() //execution starts from here
+#include <iostream>  //header file for input and output
+using namespace std; // namespace declaration
+int main()           // execution starts from here
 {
-    cout << "Hello, World!" << endl; //body of the program
+    cout << "Hello, World!" << endl; // body of the program
     return 0;
 
-// variable declaration and initialization
+    // variable declaration and initialization
     int a = 10;
     float b = 5.5;
     char c = 'A';
@@ -20,14 +20,25 @@ int main() //execution starts from here
     return 0;
 
     //  basic data types in C++
-    int integerVar = 42;               // Integer type
-    float floatVar = 3.14f;           // Floating-point type
-    double doubleVar = 2.718281828;   // Double-precision floating-point type
-    char charVar = 'C';               // Character type     
-    bool boolVar = true;              // Boolean type
-    string stringVar = "Hello, C++";  // String type
-    
+    int integerVar = 42;             // Integer type
+    float floatVar = 3.14f;          // Floating-point type
+    double doubleVar = 2.718281828;  // Double-precision floating-point type
+    char charVar = 'C';              // Character type
+    bool boolVar = true;             // Boolean type
+    string stringVar = "Hello, C++"; // String type
+
+    // derived data types
+    int arr[5] = {1, 2, 3, 4, 5}; // Array
+
+    // user-defined data types
+    struct Person
+    { // Structure
+        string name;
+        int age;
+    };
+    class Basics
+    {
+    }; // Class
 
     return 0;
 }
-
